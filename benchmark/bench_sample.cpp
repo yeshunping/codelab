0c1f60fcbe299412e561a1261c09a55ed246e686
yeshunping <yeshunping@gmail.com>
2013-05-18 13:45:21 +0800

    update sample using benchmark_main

--- benchmark/bench_sample.cpp
+++ benchmark/bench_sample.cpp
@@ -1,10 +1,10 @@
 // Copyright (c) 2013, The Toft Authors. All rights reserved.
 // Author: Ye Shunping <yeshunping@gmail.com>
 
-#include "toft/hash/hash.h"
 #include "toft/crypto/hash/md5.h"
-#include "thirdparty/re2/util/benchmark.h"
+#include "toft/hash/hash.h"
 #include "thirdparty/glog/logging.h"
+#include "thirdparty/re2/util/benchmark.h"
 
 static void Hash(int n) {
     std::string str = "aaaaaaaaaaaaaaaaaa";
@@ -24,16 +24,3 @@ static void MD5String(int n) {
 
 BENCHMARK(Hash)->ThreadRange(1, NumCPUs());
 BENCHMARK(MD5String)->ThreadRange(1, NumCPUs());
-
-extern int nbenchmarks;
-extern testing::Benchmark* benchmarks[];
-
-int main(int argc, char** argv) {
-    google::ParseCommandLineFlags(&argc, &argv, false);
-    for (int i = 0; i < nbenchmarks; i++) {
-        testing::Benchmark* b = benchmarks[i];
-        for (int j = b->threadlo; j <= b->threadhi; j++)
-            for (int k = std::max(b->lo, 1); k <= std::max(b->hi, 1); k <<= 1)
-                RunBench(b, j, k);
-    }
-}
